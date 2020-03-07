26159f8e9188e064d80ad0786d913de96fe89bcb
KisetsuLee <lizhijie_lee@126.com>
2020-03-07 14:34:39 +0800

    登录界面images动态获取

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,36 +1,67 @@
-import Mock from 'mockjs'
+import Mock from "mockjs";
 
-
-const Random = Mock.Random
-const domain = 'http://mockjs.com/api'
-const code = 200
+const Random = Mock.Random;
+const domain = "http://mockjs.com/api";
+const code = 200;
 
 const userData = function() {
-    let data;
-    data = {"code": 200, "data":{"token": "123456"}, "msg": "登陆成功"}
-    return data
-}
+  let data;
+  data = { code: 200, data: { token: "123456" }, msg: "登陆成功" };
+  return data;
+};
 
 const watchData = function() {
-    let data
-    data = {
-                "code": 200,
-                "data":{
-                    "statics": [
-                        {"type":"每日活跃数","cnt":"800","icon":"fa-arrow-up"},
-                        {"type":"每日新增数","cnt":"30","icon":"fa-arrow-up"},
-                        {"type":"总数","cnt":"1000","icon":"fa-arrow-down"},
-                        {"type":"注销用户","cnt":"20","icon":"fa-arrow-up"},
-                        {"type":"非活跃用户","cnt":"10","icon":"fa-arrow-down"},
-                        {"type":"剔除傻逼","cnt":"8","icon":"fa-arrow-down"},
-                        ],
-                    },
-                "msg": "登陆成功"
-            }
-    return data
-}
+  let data;
+  data = {
+    code: 200,
+    data: {
+      statics: [
+        { type: "每日活跃数", cnt: "800", icon: "fa-arrow-up" },
+        { type: "每日新增数", cnt: "30", icon: "fa-arrow-up" },
+        { type: "总数", cnt: "1000", icon: "fa-arrow-down" },
+        { type: "注销用户", cnt: "20", icon: "fa-arrow-up" },
+        { type: "非活跃用户", cnt: "10", icon: "fa-arrow-down" },
+        { type: "剔除傻逼", cnt: "8", icon: "fa-arrow-down" }
+      ]
+    },
+    msg: "登陆成功"
+  };
+  return data;
+};
+
+const avatorImages = () => {
+  return {
+    code: 200,
+    data: {
+      images: [
+        "/img/1.jpeg",
+        "/img/2.jpeg",
+        "/img/3.jpeg",
+        "/img/4.jpeg",
+        "/img/5.jpeg",
+        "/img/6.jpeg",
+        "/img/7.jpeg",
+        "/img/8.jpeg",
+        "/img/9.jpeg",
+        "/img/10.jpeg",
+        "/img/11.jpeg",
+        "/img/12.jpeg",
+        "/img/13.jpeg",
+        "/img/14.jpeg",
+        "/img/15.jpeg",
+        "/img/16.jpeg",
+        "/img/17.jpeg",
+        "/img/18.jpeg",
+        "/img/19.jpeg",
+        "/img/20.jpeg"
+      ]
+    }
+  };
+};
+
 // 定义请求链接，类型，还有返回数据
-Mock.mock(`${domain}/login`, 'get', userData);
+Mock.mock(`${domain}/login`, "post", userData);
 
-Mock.mock(`${domain}/watch`, 'get', watchData);
+Mock.mock(`${domain}/watch`, "get", watchData);
 
+Mock.mock(`${domain}/getImages`, "get", avatorImages);
--- src/views/public/Login.vue
+++ src/views/public/Login.vue
@@ -1,113 +1,113 @@
 <template>
-<v-container fill-height class="text-center" fluid style="background:linear-gradient(to right, #24243e, #141E30, #0f0c29);">
-     <v-col cols="12" md=6 sm=12 xs=12>
-    <v-row
-      align="center"
-      class="mx-auto white--text"
-      justify="center"
-      >
-       <p class='overline font-weight-bold grey--text' dark>Members</p>
-     </v-row>
-     <v-row
-      align="center"
-      class="mx-auto"
-      justify="center"
-      >
-      <v-col cols=10>
-             <v-divider class="mb-6" dark></v-divider>
-
-      <v-avatar color="grey lighten-4" v-for="img in images">
-                        <img :src="img" alt="avatar">
-        </v-avatar>
+  <v-container
+    fill-height
+    class="text-center"
+    fluid
+    style="background:linear-gradient(to right, #24243e, #141E30, #0f0c29);"
+  >
+    <v-col cols="12" md="6" sm="12" xs="12">
+      <v-row align="center" class="mx-auto white--text" justify="center">
+        <p class="overline font-weight-bold grey--text" dark>Members</p>
+      </v-row>
+      <v-row align="center" class="mx-auto" justify="center">
+        <v-col cols="10">
+          <v-divider class="mb-6" dark></v-divider>
+          <v-avatar color="grey lighten-4" v-for="img in images" :key="img">
+            <img :src="img" alt="avatar" />
+          </v-avatar>
         </v-col>
       </v-row>
 
-       <v-row
-      align="center"
-      class="mx-auto"
-      justify="center"
-      >
-      <v-col cols=10>
-                           <v-divider class="mx-6" dark></v-divider>
-
-        <p  class="overline grey--text mt-6">宗旨：工作中解决的问题，随手学到的知识，帮助小伙伴时的乐趣。
-        </p>
-
-      </v-col>
-         </v-row>
-    </v-col>
-    <v-col cols="12" md=6 sm=12 xs=12 >
-    <v-row
-      align="center"
-      class="mx-auto white--text"
-      justify="center"
-      >
-       <p class='headline indigo--text font-weight-bold uppercase'>RWPLUS</p>
-     </v-row>
-     <v-row
-      align="center"
-      class="mx-auto white--text"
-      justify="center"
-      >
-      <v-col cols=10>
-        <v-text-field  dark dense color='indigo' prepend-icon="fa-user-check" label="电子邮件"  v-model="model.username"  placeholder="rwplus@github.com"></v-text-field>
-        </v-text-field>
+      <v-row align="center" class="mx-auto" justify="center">
+        <v-col cols="10">
+          <v-divider class="mx-6" dark></v-divider>
+          <p class="overline grey--text mt-6">
+            宗旨：工作中解决的问题，随手学到的知识，帮助小伙伴时的乐趣。
+          </p>
         </v-col>
-        </v-row>
-            <v-row
-            align="center"
-            class="mx-auto white--text"
-            justify="center"
-         > <v-col cols=10>
-        <v-text-field  dark dense color='indigo' prepend-icon="fa-key" label="密码" type="password" v-model="model.password" placeholder="password">
-        </v-text-field>
-        </v-col>
-        </v-row>
-         <v-btn small dark dense  outlined rounded dark @click="login" color="deep-orange">登录</v-btn>
-         <v-btn small dark dense color="indigo"  rounded class="ml-6">注册</v-btn>
+      </v-row>
     </v-col>
-</v-container>
+    <v-col cols="12" md="6" sm="12" xs="12">
+      <v-row align="center" class="mx-auto white--text" justify="center">
+        <p class="headline indigo--text font-weight-bold uppercase">RWPLUS</p>
+      </v-row>
+
+      <v-row align="center" class="mx-auto white--text" justify="center">
+        <v-col cols="10">
+          <v-text-field
+            dark
+            dense
+            color="indigo"
+            prepend-icon="fa-user-check"
+            label="电子邮件"
+            v-model="model.username"
+            placeholder="rwplus@github.com"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" class="mx-auto white--text" justify="center">
+        <v-col cols="10">
+          <v-text-field
+            dark
+            dense
+            color="indigo"
+            prepend-icon="fa-key"
+            label="密码"
+            type="password"
+            v-model="model.password"
+            placeholder="password"
+          >
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn small dark dense outlined rounded @click="login" color="deep-orange">登录</v-btn>
+      <v-btn small dark dense color="indigo" rounded class="ml-6">注册</v-btn>
+    </v-col>
+  </v-container>
 </template>
-
-
-
 
 <script>
 export default {
-    data: () => ({
+  data: () => ({
     loading: false,
     model: {
-      username: 'weeknd.su@wukongbox.com',
-      password: '123456'
+      username: "weeknd.su@wukongbox.com",
+      password: "123456"
     },
-    images: ['/img/1.jpeg','/img/2.jpeg','/img/3.jpeg','/img/4.jpeg','/img/5.jpeg','/img/6.jpeg','/img/7.jpeg','/img/8.jpeg','/img/9.jpeg','/img/10.jpeg',
-     '/img/11.jpeg','/img/12.jpeg','/img/13.jpeg','/img/14.jpeg','/img/15.jpeg','/img/16.jpeg','/img/17.jpeg','/img/18.jpeg','/img/19.jpeg','/img/20.jpeg'
-    ],
+    images: []
   }),
   methods: {
     login() {
-        let that=this;
-        this.$http.get("/login").then(res => {
-            if (res.data.code === 200) {
-                this.$store.commit("snackbar/setSnackbar", {
-                    show: true,
-                    message: res.data.msg,
-                    color: "black",
-                    top: true
-                });
-            } else {
-                this.$store.commit("snackbar/setSnackbar", {
-                    show: true,
-                    message: res.data.msg,
-                    color: "error",
-                    top: true
-                });
-            }
-        });
+      let that = this;
+      this.$http.get("/login").then(res => {
+        if (res.data.code === 200) {
+          this.$store.commit("snackbar/setSnackbar", {
+            show: true,
+            message: res.data.msg,
+            color: "black",
+            top: true
+          });
+        } else {
+          this.$store.commit("snackbar/setSnackbar", {
+            show: true,
+            message: res.data.msg,
+            color: "error",
+            top: true
+          });
+        }
+      });
     },
-    show(){
-
-    },
+    getImages() {
+      this.$http.get("getImages").then(res => {
+        if (res.data.code === 200) {
+          this.images = res.data.data.images;
+        }
+      });
+    }
+  },
+  created() {
+    this.getImages();
   }
-}
+};
 </script>
